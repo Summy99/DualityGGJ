911653dc01d838ca0e72dfa592388ced50de782a
Summy99 <dylan_hensley@bloomfield.edu>
2022-01-28 21:10:54 -0500

    gun is animated :)

---
+++ Assets/Sumner's Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator anim;
+    private AnimationState state;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("fire");
+        }
+    }
+}
