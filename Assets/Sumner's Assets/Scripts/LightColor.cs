aa3436dc07082cf8505d1fdae75f61f1b4aa2ffe
Summy99 <dylan_hensley@bloomfield.edu>
2022-01-29 09:48:44 -0500

    walls and moar rainbows

---
+++ Assets/Sumner's Assets/Scripts/LightColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightColor : MonoBehaviour
+{
+    private Light sun;
+
+    public float hue = 0f;
+    private string change = "up";
+
+    public float roc = 0.1f;
+
+    void Start()
+    {
+        sun = GameObject.FindGameObjectWithTag("Sun").GetComponent<Light>();
+    }
+
+    void FixedUpdate()
+    {
+        sun.color = Color.HSVToRGB(hue, 0.35f, 1);
+
+        if(hue <= 0)
+            change = "up";
+
+        if (hue >= 1)
+            change = "down";
+
+        if (change == "down")
+            hue -= roc;
+
+        if (change == "up")
+            hue += roc;
+
+    }
+}
