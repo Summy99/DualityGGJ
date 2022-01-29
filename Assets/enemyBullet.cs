529cad68990ac5e5e349ae8b0d6514eb7fab449b
JSoilder4 <winkler21960@gmail.com>
2022-01-29 14:13:32 -0500

    DEBIL SHOOOOOTY HAHA :pray: :gun:

--- Assets/enemyBullet.cs
+++ Assets/enemyBullet.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class enemyBullet : MonoBehaviour
 {
     public GameObject player;
+    public Rigidbody rb;
+    public float speed = 25f;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         transform.LookAt(player.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(gameObject, 2f);
+    }
+    private void FixedUpdate()
+    {
+            rb.velocity = transform.forward * speed;
     }
 }
