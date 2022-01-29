5450fdcb23f5374247abd83f6bf180fa23adcb62
JSoilder4 <winkler21960@gmail.com>
2022-01-28 19:37:19 -0500

    basic script start

--- Assets/John's Assets/2D Movement.cs
+++ Assets/John's Assets/2D Movement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
+    public Vector3 velocity;
+    public float speed;
+    public Rigidbody2D rb;
+
+    public bool dead;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!dead)
+        {
+            velocity.x = Input.GetAxisRaw("Horizontal");// * speed;
+            velocity.y = Input.GetAxisRaw("Vertical");// * speed;
+
+            velocity = Vector3.Normalize(velocity);
+            velocity *= speed;
+
+            rb.MovePosition(transform.position + velocity * Time.deltaTime);
+        }
+
+
     }
 }
