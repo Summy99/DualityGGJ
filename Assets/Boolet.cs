213f650005e10c8c090b84241d0b4eded1bc04bb
Summy99 <dylan_hensley@bloomfield.edu>
2022-01-28 22:20:24 -0500

    you can shoot now :gun:

---
+++ Assets/Boolet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boolet : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = transform.up * speed;
+    }
+}
--- Assets/Sumner's Assets/Scripts/PlayerController.cs
+++ Assets/Sumner's Assets/Scripts/PlayerController.cs
@@ -6,17 +6,29 @@ public class PlayerController : MonoBehaviour
 {
     private Animator anim;
     private AnimationState state;
+    public GameObject boolet;
+    private GameObject cameraRef;
+
+    private GameObject booletSpawned;
+
     void Start()
     {
         anim = GetComponent<Animator>();
-        
+        cameraRef = transform.GetChild(0).gameObject;
     }
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            anim.SetTrigger("fire");
+            Shoot();
         }
+    }
+
+    private void Shoot()
+    {
+        anim.SetTrigger("fire");
+        booletSpawned = Instantiate(boolet, transform.GetChild(0).GetChild(0).GetChild(0).position, Quaternion.Euler(cameraRef.transform.rotation.eulerAngles.x + 90, cameraRef.transform.rotation.eulerAngles.y, cameraRef.transform.rotation.eulerAngles.z));
+        Destroy(booletSpawned, 10f);
     }
 }
