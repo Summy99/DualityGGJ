b958feae4f94c0fb7bf4f03c3f578fd5fefb1507
JSoilder4 <winkler21960@gmail.com>
2022-01-30 12:12:39 -0500

    quitbutton!?!?!

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject pause;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,13 @@ public class MainMenuScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                pause.SetActive(!pause.activeSelf);
+            }
+        }
     }
     public void play()
     {
