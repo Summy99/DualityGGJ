d59a841571da7debf32f83b2ec6ac4a98d406a9e
JSoilder4 <winkler21960@gmail.com>
2022-01-30 12:38:05 -0500

    lolSoRandomAndQuirkyX3

---
+++ Assets/signalForFalling.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class signalForFalling : MonoBehaviour
+{
+    public Rigidbody rbGun;
+    public Rigidbody rbGlasses;
+
+
+    public void fall()
+    {
+        rbGun.useGravity = true;
+        rbGlasses.useGravity = true;
+    }
+}
