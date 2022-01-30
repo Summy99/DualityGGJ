af436ecf371cf8677ea169b294949d4aa4d5cae5
JSoilder4 <winkler21960@gmail.com>
2022-01-30 11:47:02 -0500

    HOLY SHIRT THE TITLE SCREEN IS DONE HAHA FUNNY REFERENCE TEEHEEHEE

---
+++ Assets/menuOn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuOn : MonoBehaviour
+{
+    public Transform vmCam;
+    public Camera turnoff;
+    public Camera turnon;
+    public GameObject mainmenuCanvas;
+    public void menuTurnOn()
+    {
+        turnoff.enabled = false;
+        turnon.enabled = true;
+        mainmenuCanvas.SetActive(true);
+
+       // transform.position = vmCam.position;
+       //  transform.rotation = vmCam.rotation;
+        print("eyyo this script be workin dog");
+    }
+}
