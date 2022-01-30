0a1bf0482e2a868d5c4fc5f5e958acb5193dc642
Summy99 <dylan_hensley@bloomfield.edu>
2022-01-30 14:27:22 -0500

    ready to build

--- Assets/Sumner's Assets/Scripts/Splatter.cs
+++ Assets/Sumner's Assets/Scripts/Splatter.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Splatter : MonoBehaviour
 {
     private AudioSource src;
+    public GameObject smoke;
     public GameObject bloodSplat;
+
+    private bool burning = false;
 
     private void Start()
     {
         src = GetComponent<AudioSource>();
+        burning = true;
+        Instantiate(smoke, transform.position, Quaternion.Euler(-90, 0, 0), transform);
     }
 
     private void OnCollisionEnter(Collision collision)
---
+++ Assets/soke.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soke : MonoBehaviour
+{
+    Quaternion tr;
+    void Start()
+    {
+        tr = transform.rotation;
+    }
+
+    void Update()
+    {
+        transform.rotation = tr;
+    }
+}
