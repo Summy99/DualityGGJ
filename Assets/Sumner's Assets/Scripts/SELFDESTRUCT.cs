4da7b7864a1da9081969814b1e064ee24e85116b
Summy99 <dylan_hensley@bloomfield.edu>
2022-01-29 22:30:12 -0500

    gibs work now :+1:

---
+++ Assets/Sumner's Assets/Scripts/SELFDESTRUCT.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SELFDESTRUCT : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(SELF_DESTRUCT());
+    }
+
+    IEnumerator SELF_DESTRUCT()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Sumner's Assets/Scripts/Splatter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splatter : MonoBehaviour
+{
+    public GameObject bloodSplat;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(bloodSplat,transform.position, Quaternion.identity);
+    }
+}
